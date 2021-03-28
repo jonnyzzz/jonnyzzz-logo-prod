10590c4ee41ebfcfca774219aca7844f164e3f07
Eugene Petrenko <me@jonnyzzz.com>
2021-03-28 15:34:49 +0200

    more fixes

--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e -x -u
 
-cd HOME="$(pwd)"
+HOME="$(pwd)"
+ROOT="$(cd ../jonnyzzz-logo && pwd)"
 
 export JAVA_HOME=/Users/jonnyzzz/Library/Java/JavaVirtualMachines/corretto-11.0.10/Contents/Home
 
 
-cd ../jonnyzzz-logo && ./gradlew clean :logo-web:build
+cd $ROOT && ./gradlew clean :logo-web:build
 
-rsync -ai ../jonnyzzz-logo/logo-web/build/distributions/** .
+rsync -ai $ROOT/logo-web/build/distributions $HOME
 git add -A .
 
 
