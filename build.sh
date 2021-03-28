06d58dde6bd9f860dcea893decbe096de97bed54
Eugene Petrenko <me@jonnyzzz.com>
2021-03-28 15:27:39 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e -x -u
+
+export JAVA_HOME=/Users/jonnyzzz/Library/Java/JavaVirtualMachines/corretto-11.0.10/Contents/Home
+
+
+../jonnyzzz-logo/gradlew :logo-web:build
+
+rsync -ai --delete  ../jonnyzzz-logo/logo-web/build/distributions/** .
+git add -A .
+
+
+
+
