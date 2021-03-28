e904878d05e416e385014236245cc6ab2d7fd853
Eugene Petrenko <me@jonnyzzz.com>
2021-03-28 15:37:43 +0200

    update

--- build.sh
+++ build.sh
@@ -10,9 +10,11 @@ ROOT="$(cd ../jonnyzzz-logo && pwd)"
 export JAVA_HOME=/Users/jonnyzzz/Library/Java/JavaVirtualMachines/corretto-11.0.10/Contents/Home
 
 
-cd $ROOT && ./gradlew clean :logo-web:build
+cd $ROOT 
+./gradlew clean :logo-web:build
+cd $HOME
 
-rsync -ai $ROOT/logo-web/build/distributions $HOME
+rsync -ai $ROOT/logo-web/build/distributions/ $HOME
 git add -A .
 
 
