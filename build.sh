8fcd01ebdfbb3e9b3509a15fafdac526f80b232a
Eugene Petrenko <me@jonnyzzz.com>
2021-03-28 15:31:25 +0200

    fixing

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e -x -u
 
+cd HOME="$(pwd)"
+
 export JAVA_HOME=/Users/jonnyzzz/Library/Java/JavaVirtualMachines/corretto-11.0.10/Contents/Home
 
 
-../jonnyzzz-logo/gradlew :logo-web:build
+cd ../jonnyzzz-logo && ./gradlew clean :logo-web:build
 
 rsync -ai ../jonnyzzz-logo/logo-web/build/distributions/** .
 git add -A .
-
-
 
 
